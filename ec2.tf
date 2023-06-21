12030975e8a2e8790aa8c2e871afc3cbf007a97a
swathidhanu <81679244+Swathi0123@users.noreply.github.com>
2023-06-21 18:46:46 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,6 +6,14 @@ resource "aws_instance" "ec2" {
    // security_groups = ["rtp03-sg"]
    vpc_security_group_ids = ["${aws_security_group.rtp03-sg.id}"]
    subnet_id = "${aws_subnet.rtp03-public_subent_01.id}"
+    user_data              = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+              echo "Hello World" > /var/www/html/index.html
+              systemctl restart apache2
+              EOF
    for_each = toset(["Dev", "Prod"])
    tags = {
      Name = "${each.key}"
