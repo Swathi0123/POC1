a16746d5d6f80e2be73be6e50e6dd97b6bcf8d20
swathidhanu <81679244+Swathi0123@users.noreply.github.com>
2023-06-21 13:42:56 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "ami" {
+    default = "ami-022e1a32d3f742bd8"  
+}
+
+variable "associate_ip" {
+    default = true
+  
+}
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "key-name" {
+    default = "kp_12"
+}
+
+variable "vpc_cidr" {
+    default = "10.3.0.0/16"
+}
+
+variable "subnet_cidr" {
+    default = "10.3.1.0/24"
+}
