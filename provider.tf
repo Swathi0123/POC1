5b2a3ef80844646cdb49cc0bd69e95fec77f2a7e
swathidhanu <81679244+Swathi0123@users.noreply.github.com>
2023-06-22 12:30:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ terraform {
   backend "s3" {
     bucket = "tf-state-terraform1"
     region = "us-east-1"
-    key    = "dev/terraform.tfstate"
+    key    = "terraform.tfstate"
     #profile = "default"
   }
   }
