a2863587c06ad6e5d463e4ce637cb050d270e5cf
swathidhanu <81679244+Swathi0123@users.noreply.github.com>
2023-06-22 15:28:03 +0530

    Update SG.tf

--- SG.tf
+++ SG.tf
@@ -30,33 +30,3 @@ resource "aws_security_group" "rtp03-sg" {
 }
 
 
-resource "aws_security_group" "rtp03-sg1" {
-    name = "rtp03-sg1"
-    vpc_id = "${aws_vpc.rtp03-vpc.id}"
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-
-    }
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    tags = {
-        Name = "ssh-sg"
-
-    }
-
-}
