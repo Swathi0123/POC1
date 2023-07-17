85ec919ea7e0b7e0af1d22a4b445872a0d988757
swathidhanu <81679244+Swathi0123@users.noreply.github.com>
2023-07-17 13:33:05 +0530

    Update iam_role.tf

--- iam_role.tf
+++ iam_role.tf
@@ -19,17 +19,17 @@ POLICY
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.master.name
+  role       = aws_iam_role.master1.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = aws_iam_role.master.name
+  role       = aws_iam_role.master1.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.master.name
+  role       = aws_iam_role.master1.name
 }
 
 resource "aws_iam_role" "worker1" {
