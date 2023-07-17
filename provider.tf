564e6ac3a4b2ae0c58874f6256e738f4c6cc1557
swathidhanu <81679244+Swathi0123@users.noreply.github.com>
2023-07-17 12:12:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,3 +10,13 @@ terraform {
 provider "aws" {
   region = "ap-southeast-1"
 }
+
+
+terraform {
+  backend "s3" {
+    bucket         = "poceks1"
+    key            = "terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "poceks1"
+  }
+}
